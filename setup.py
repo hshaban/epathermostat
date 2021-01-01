31b7597b3a4f032142fe15be587471b8ec94dd29
Hassan Shaban <hassanshaban@gmail.com>
2021-01-01 14:39:59 -0800

    Updated eeweather version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='thermostat',
     package_data={'': ['*.csv', '*.json']},
     install_requires=[
         'eemeter==2.5.2',
-        'eeweather==0.3.23',
+        'eeweather==0.3.24',
         'pandas==0.25.3',
         'sqlalchemy==1.3.1',
         'zipcodes==1.1.2',
